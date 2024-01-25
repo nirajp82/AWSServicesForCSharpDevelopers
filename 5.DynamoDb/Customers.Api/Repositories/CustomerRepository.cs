b8178b2daf1b8ba9da2be987d0cab1a96f9584d7
Niraj Patel <nirajp82@yahoo.com>
2024-01-24 23:09:09 -0500

    Updated customer repository

--- 5.DynamoDb/Customers.Api/Repositories/CustomerRepository.cs
+++ 5.DynamoDb/Customers.Api/Repositories/CustomerRepository.cs
@@ -1,10 +1,7 @@
 ﻿using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DocumentModel;
 using Amazon.DynamoDBv2.Model;
-using Amazon.DynamoDBv2.Model.Internal.MarshallTransformations;
-using Amazon.Runtime.Internal.Transform;
 using Customers.Api.Contracts.Data;
-using Microsoft.Extensions.Options;
 using System.Net;
 
 using System.Text.Json;
@@ -14,36 +11,51 @@ namespace Customers.Api.Repositories;
 public class CustomerRepository : ICustomerRepository
 {
     private readonly IAmazonDynamoDB _dynamoDB;
-    const string _tableName = "customers";
+    private const string _tableName = "customers";
 
+    // Constructor to initialize the DynamoDB client
     public CustomerRepository(IAmazonDynamoDB dynamoDB)
     {
         _dynamoDB = dynamoDB;
     }
 
+    // Create a new customer asynchronously
     public async Task<bool> CreateAsync(CustomerDto customer, CancellationToken cancellationToken)
     {
+        // Set the updated timestamp
         customer.UpdatedAt = DateTime.UtcNow;
+
+        // Serialize customer object to JSON and convert to DynamoDB attribute map
         var custAsJson = JsonSerializer.Serialize(customer);
         var custAttributes = Document.FromJson(custAsJson).ToAttributeMap();
+
+        // Prepare the PutItem request with conditional expression
         var createItemReq = new PutItemRequest
         {
             TableName = _tableName,
             Item = custAttributes,
             ConditionExpression = "attribute_not_exists(pk) and attribute_not_exists(sk)"
         };
+
+        // Execute PutItem request and return success status
         var response = await _dynamoDB.PutItemAsync(createItemReq, cancellationToken);
         return response.HttpStatusCode == HttpStatusCode.OK;
     }
 
+    // Get customer by ID asynchronously
     public async Task<CustomerDto?> GetAsync(Guid id, CancellationToken cancellationToken)
     {
+        // Convert Guid to string
         var custId = id.ToString();
+
+        // Prepare GetItem request with primary key attributes
         var getItemRequest = new GetItemRequest(_tableName, new Dictionary<string, AttributeValue>
         {
             { "pk", new AttributeValue(custId) },
             { "sk", new AttributeValue(custId) }
         });
+
+        // Execute GetItem request and deserialize response to CustomerDto
         var response = await _dynamoDB.GetItemAsync(getItemRequest, cancellationToken);
         if (response.Item.Count == 0)
             return null;
@@ -52,12 +64,16 @@ public class CustomerRepository : ICustomerRepository
         return JsonSerializer.Deserialize<CustomerDto?>(itemDoc.ToJson());
     }
 
+    // Get all customers asynchronously
     public async Task<IEnumerable<CustomerDto>> GetAllAsync(CancellationToken cancellationToken)
     {
+        // Prepare Scan request to retrieve all items from the table
         var scanRequest = new ScanRequest()
         {
             TableName = _tableName
         };
+
+        // Execute Scan request and deserialize items to CustomerDto list
         var response = await _dynamoDB.ScanAsync(scanRequest, cancellationToken);
         var customers = response.Items?.Select(item =>
         {
@@ -67,28 +83,50 @@ public class CustomerRepository : ICustomerRepository
         return customers!;
     }
 
+    // Update customer asynchronously with optimistic locking
     public async Task<bool> UpdateAsync(CustomerDto customer, DateTime requestStarted, CancellationToken cancellationToken)
     {
+        // Set the updated timestamp
         customer.UpdatedAt = DateTime.UtcNow;
+
+        // Serialize customer object to JSON and convert to DynamoDB attribute map
         var custAsJson = JsonSerializer.Serialize(customer);
         var custAttributes = Document.FromJson(custAsJson).ToAttributeMap();
+
+        // Prepare PutItem request with conditional expression for optimistic locking
         var updateItemReq = new PutItemRequest
         {
             TableName = _tableName,
             Item = custAttributes,
+            
+            // Specify a condition for the DynamoDB update operation using optimistic locking.
+            // This condition ensures that the update will only be performed if the 'UpdatedAt'
+            // attribute in the database is less than the 'requestStarted' timestamp.
+
+            // ConditionExpression defines the condition for the update operation.
+            // It checks if the 'UpdatedAt' attribute in the item is less than the specified timestamp.
             ConditionExpression = "UpdatedAt < :requestStarted",
+
+            // ExpressionAttributeValues provides the values to be substituted into the ConditionExpression.
+            // In this case, it includes a placeholder ":requestStarted" with the corresponding timestamp value.
             ExpressionAttributeValues = new Dictionary<string, AttributeValue>
             {
                 { ":requestStarted", new AttributeValue{S = requestStarted.ToString("O")} }
             }
         };
+
+        // Execute PutItem request and return success status
         var response = await _dynamoDB.PutItemAsync(updateItemReq, cancellationToken);
         return response.HttpStatusCode == HttpStatusCode.OK;
     }
 
+    // Delete customer by ID asynchronously
     public async Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken)
     {
+        // Convert Guid to string
         var custId = id.ToString();
+
+        // Prepare DeleteItem request with primary key attributes
         var deleteItemRequest = new DeleteItemRequest
         {
             TableName = _tableName,
@@ -98,6 +136,8 @@ public class CustomerRepository : ICustomerRepository
                 {"sk",new AttributeValue(custId) }
             }
         };
+
+        // Execute DeleteItem request and return success status
         var response = await _dynamoDB.DeleteItemAsync(deleteItemRequest, cancellationToken);
         return response.HttpStatusCode == HttpStatusCode.OK;
     }
