d02cf4943b2c110010e3d6a7dcc73d06b65263f9
Niraj Patel <nirajp82@yahoo.com>
2024-02-18 16:39:50 -0500

    Completed Simpe Lambda Function

--- 8.Lambda/SimpleLambda/Functions.cs
+++ 8.Lambda/SimpleLambda/Functions.cs
@@ -9,13 +9,15 @@ using Amazon.Lambda.Annotations.APIGateway;
 
 namespace SimpleLambda;
 
+//Deploy: dotnet lambda deploy-function SimpleLambda
+//Run: dotnet lambda invoke-function SimpleLambda --payload '{ ""Name"": ""ShreeRajShyamaji""}'
 public class Functions
 {
-    /// <summary>
-    /// Default constructor that Lambda will invoke.
-    /// </summary>
-    public Functions()
+    [LambdaFunction(Role = "MyLambda-role-2iccehky", MemorySize = 256, Timeout = 30)]
+    public string FunctionHandler(LambdaRequest request, ILambdaContext context)
     {
+        context.Logger.LogInformation($"Pranam from AWS Serverless Application -> Empty Serverless Application. Request Data: {request.Name}");
+        return $"Pranam, {request.Name}!!!";
     }
 
     /*
@@ -43,9 +45,61 @@ public class Functions
             return HttpResults.Ok("Hello AWS Serverless");
         }
     */
-    [LambdaFunction(Role = "MyLambda-role-2iccehky", MemorySize = 256, Timeout = 30)]
-    public void FunctionHandler(ILambdaContext context)
-    {
-        context.Logger.LogInformation("Pranam from AWS Serverless Application -> Empty Serverless Application");
-    }
 }
+
+public class LambdaRequest
+{
+    public string Name { get; set; } = default!;
+}
+
+/*
+     Deploy: dotnet lambda deploy-function SimpleLambda
+     Output:
+            Updating code for existing function SimpleLambda
+            Updating function url config: https://z3wrv5feb7v2rfckz2wfbyhclm0gipkz.lambda-url.us-east-1.on.aws/
+            PS C:\Projects\AWSServicesForCSharpDevelopers\8.Lambda\SimpleLambda> dotnet lambda deploy-function SimpleLambda
+            Amazon Lambda Tools for .NET Core applications (5.10.0)
+            Project Home: https://github.com/aws/aws-extensions-for-dotnet-cli, https://github.com/aws/aws-lambda-dotnet
+
+            Executing publish command
+            Deleted previous publish folder
+            ... invoking 'dotnet publish', working folder 'C:\Projects\AWSServicesForCSharpDevelopers\8.Lambda\SimpleLambda\bin\Release\net6.0\publish'
+            ... dotnet publish "C:\Projects\AWSServicesForCSharpDevelopers\8.Lambda\SimpleLambda" --output "C:\Projects\AWSServicesForCSharpDevelopers\8.Lambda\SimpleLambda\bin\Release\net6.0\publish" --configuration "Release" --framework "net6.0" /p:GenerateRuntimeConfigurationFiles=true --runtime linux-x64 --self-contained False
+            ... publish: MSBuild version 17.8.3+195e7f5a3 for .NET
+            ... publish:   Determining projects to restore...
+            ... publish:   All projects are up-to-date for restore.
+            ... publish:   SimpleLambda -> C:\Projects\AWSServicesForCSharpDevelopers\8.Lambda\SimpleLambda\bin\Release\net6.0\linux-x64\SimpleLambda.dll
+            ... publish:   SimpleLambda -> C:\Projects\AWSServicesForCSharpDevelopers\8.Lambda\SimpleLambda\bin\Release\net6.0\publish\
+            Zipping publish folder C:\Projects\AWSServicesForCSharpDevelopers\8.Lambda\SimpleLambda\bin\Release\net6.0\publish to C:\Projects\AWSServicesForCSharpDevelopers\8.Lambda\SimpleLambda\bin\Release\net6.0\SimpleLambda.zip
+            ... zipping: Amazon.Lambda.Annotations.dll
+            ... zipping: Amazon.Lambda.APIGatewayEvents.dll
+            ... zipping: Amazon.Lambda.Core.dll
+            ... zipping: Amazon.Lambda.Serialization.SystemTextJson.dll
+            ... zipping: Microsoft.Extensions.DependencyInjection.Abstractions.dll
+            ... zipping: Microsoft.Extensions.DependencyInjection.dll
+            ... zipping: SimpleLambda.deps.json
+            ... zipping: SimpleLambda.dll
+            ... zipping: SimpleLambda.pdb
+            ... zipping: SimpleLambda.runtimeconfig.json
+            Created publish archive (C:\Projects\AWSServicesForCSharpDevelopers\8.Lambda\SimpleLambda\bin\Release\net6.0\SimpleLambda.zip).
+            Updating code for existing function SimpleLambda
+            Updating function url config: https://z3wrv5feb7v2rfckz2wfbyhclm0gipkz.lambda-url.us-east-1.on.aws/
+ */
+
+/*
+    Invoke:  dotnet lambda invoke-function SimpleLambda --payload '{ ""Name"": ""ShreeRajShyamaji""}'
+    Output:
+            Amazon Lambda Tools for .NET Core applications (5.10.0)
+            Project Home: https://github.com/aws/aws-extensions-for-dotnet-cli, https://github.com/aws/aws-lambda-dotnet
+
+            Payload:
+            "Pranam, ShreeRajShyamaji!!!"
+
+            Log Tail:
+            START RequestId: b32078a1-b16a-4637-a205-295a9940c12d Version: $LATEST
+            2024-02-18T21:38:06.914Z        b32078a1-b16a-4637-a205-295a9940c12d    info   
+                        Pranam from AWS Serverless Application -> Empty Serverless Application. Request Data: ShreeRajShyamaji
+            END RequestId: b32078a1-b16a-4637-a205-295a9940c12d
+            REPORT RequestId: b32078a1-b16a-4637-a205-295a9940c12d  Duration: 61.46 ms      
+                        Billed Duration: 62 ms  Memory Size: 256 MB     Max Memory Used: 61 MB  
+ */
